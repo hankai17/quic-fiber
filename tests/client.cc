9a3d0f47f84e63ea076193ade96f78869e829a52
hankai17 <867614535@qq.com>
2022-06-25 10:00:45 +0800

    Update client.cc

--- tests/client.cc
+++ tests/client.cc
@@ -25,7 +25,7 @@ Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
 std::string to_hex(const std::string& str) {
     std::stringstream ss;
-    for(size_t i = 0; i < str.size(); ++i) {
+    for (size_t i = 0; i < str.size(); ++i) {
         ss << std::setw(2) << std::setfill('0') << std::hex
            << (int)(uint8_t)str[i];
     }
@@ -35,8 +35,9 @@ std::string to_hex(const std::string& str) {
 int file_size(const char* filePath) {
     if(filePath == NULL) return 0;
     struct stat sb;
-    if(stat(filePath, &sb) < 0)
-    return 0;
+    if (stat(filePath, &sb) < 0) {
+        return 0;
+    }
     return sb.st_size;
 }
 
@@ -67,9 +68,9 @@ void client_send_file(QuicServer::ptr server)
     int size = 0;
     auto server_addr = sylar::IPv4Address::Create("0.0.0.0", 4242);
     auto client_session = server->newClientSession(server_addr);
-    SYLAR_LOG_ERROR(g_logger) << "session id: " << client_session->getCid()->toHexString();
     auto stream = client_session->openStream();
-    SYLAR_LOG_ERROR(g_logger) << "streamid: " << stream->stream_id();
+    SYLAR_LOG_ERROR(g_logger) << "session id: " << client_session->getCid()->toHexString()
+                              << ", stream id: " << stream->stream_id();
 
     size = file_size(TMP_FILE);
     if (size <= 0) {
@@ -92,13 +93,14 @@ void client_send_file(QuicServer::ptr server)
         buffer_block->write(buffer.c_str(), ret);
         auto res = stream->write(buffer_block);
         if (res->bytes_rw() != ret || res->err_no() != 0) {
-            SYLAR_LOG_ERROR(g_logger) << "ret: " << ret << ", write ret: " << res->bytes_rw() << ", error: " << res->err_no() << ", strerr: " << res->strerr();
+            SYLAR_LOG_ERROR(g_logger) << "ret: " << ret << ", write ret: " << res->bytes_rw() 
+                                      << ", error: " << res->err_no() << ", strerr: " << res->strerr();
         }
         sum += res->bytes_rw();
     }
     SYLAR_LOG_ERROR(g_logger) << "sum: " << sum << ", remain size: " << size;
+                              << ", " << stream->toSndStatisticsString();
     fclose(f);
-    SYLAR_LOG_ERROR(g_logger) << stream->toSndStatisticsString();
     stream->close();
 }
 
